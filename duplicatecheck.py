2562d76ea20171188457bb2ddd6e800534fb2a5b
Kenny Chong <kenny.chong@u.nus.edu>
2018-05-15 16:44:56 +0800

    Add files via upload

---
+++ duplicatecheck.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+#day 9
+frm = 144038905
+to = 184903890
+
+#day 8
+frm2 =
+to2 =
+
+dtypes = {
+        'ip'            : 'uint32',
+        'app'           : 'uint16',
+        'device'        : 'uint16',
+        'os'            : 'uint16',
+        'channel'       : 'uint16',
+        'is_attributed' : 'uint8',
+        'click_id'      : 'uint32',
+        }
+
+full = train_df = pd.read_csv("train.csv", parse_dates=['click_time'],
+                       dtype=dtypes, usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed'])
+
+
+train_df = pd.read_csv("train.csv", parse_dates=['click_time'], skiprows=range(1, frm), nrows=to - frm,
+                       dtype=dtypes, usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed'])
+train_df['hour'] = pd.to_datetime(train_df.click_time).dt.hour.astype('uint8')
+train_df['day'] = pd.to_datetime(train_df.click_time).dt.day.astype('uint8')
+train_df['minute'] = pd.to_datetime(train_df.click_time).dt.minute.astype('uint8')
+train_df['seconds'] = pd.to_datetime(train_df.click_time).dt.second.astype('uint8')
+
+
+hey = train_df[train_df.duplicated(subset = ['ip','app','os','channel','device','day','hour','minute','seconds'],keep = False)]
+
+train_df['duplicate'] = 0
+train_df.loc[train_df.duplicated(subset = ['ip','app','os','channel','device','day','hour','minute','seconds'],keep = False),'duplicate']=1
